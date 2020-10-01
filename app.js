59acc1ae20affa96f447ff0120834b90e746fd2f
Cesar Preza <cpreza93@yahoo.com>
2020-10-01 10:56:15 -0700

    Added delete button

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 const submitButton = document.getElementById("submit-button");
 submitButton.addEventListener('click', userData);
 
-function userData() {
-    const payment = document.getElementById("payment-type").value
-    const amountPaid = document.getElementById("amount").value
-    const itemPurchased = document.getElementById("expense").value
-    const datePurchased = document.getElementById("date").value
-    const tableData = document.getElementById("table-body");
-    let newRow = `
+function userData(e) {
+  e.preventDefault();
+  const payment = document.getElementById("payment-type").value
+  const amountPaid = document.getElementById("amount").value
+  const itemPurchased = document.getElementById("expense").value
+  const datePurchased = document.getElementById("date").value
+  const tableData = document.getElementById("table-body");
+  const deleteExpense = "<button class='deleteButton'>Delete</button>";
+  deleteExpense.addEventListener('click', (e) => {
+    const deleteData = e.target.parentElement;
+    deleteData.remove();
+  });
+  let newRow = `
     <tr>
       <td>${payment}</td>
       <td>${amountPaid}</td>
       <td>${itemPurchased}</td>
       <td>${datePurchased}</td>
+      <td>${deleteExpense}</td>
     </tr>
 `
-    tableData.innerHTML = newRow;
+  tableData.innerHTML += newRow;
 
 }
