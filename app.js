8e899f98015f5f3a31521e427549105f14117301
Cesar Preza <cpreza93@yahoo.com>
2020-10-03 19:27:20 -0700

    delete button

--- app.js
+++ app.js
@@ -1,27 +1,26 @@
+const payment = document.getElementById("payment-type");
+const amountPaid = document.getElementById("amount");
+const itemPurchased = document.getElementById("expense");
+const datePurchased = document.getElementById("date");
+const deleteRow = document.createElement('button');
+deleteRow.setAttribute('id', 'deleteButton');
 const submitButton = document.getElementById("submit-button");
 submitButton.addEventListener('click', userData);
 
 function userData(e) {
   e.preventDefault();
-  const payment = document.getElementById("payment-type").value
-  const amountPaid = document.getElementById("amount").value
-  const itemPurchased = document.getElementById("expense").value
-  const datePurchased = document.getElementById("date").value
+  const type = payment.value;
+  const paid = amountPaid.value;
+  const item = itemPurchased.value;
+  const date = datePurchased.value;
   const tableData = document.getElementById("table-body");
-  const deleteExpense = "<button class='deleteButton'>Delete</button>";
-  //event listener breaks submit button
-  deleteExpense.addEventListener('click', (e) => {
-    const deleteData = e.target.parentElement;
-    deleteData.remove();
-  });
-  //Submit form button works removing the above event listener.
   let newRow = `
     <tr>
-      <td>${payment}</td>
-      <td>${amountPaid}</td>
-      <td>${itemPurchased}</td>
-      <td>${datePurchased}</td>
-      <td>${deleteExpense}</td>
+      <td>${type}</td>
+      <td>${paid}</td>
+      <td>${item}</td>
+      <td>${date}</td>
+      <td>${deleteRow}</td>
     </tr>
 `
   tableData.innerHTML += newRow;
