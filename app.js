5719ed5d40bc5c9d6426aa5ca0a51b904a81a031
Cesar Preza <cpreza93@yahoo.com>
2020-10-05 19:02:20 -0700

    Delete Button changes

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const payment = document.getElementById("payment-type");
 const amountPaid = document.getElementById("amount");
 const itemPurchased = document.getElementById("expense");
 const datePurchased = document.getElementById("date");
-const deleteRow = document.createElement('button');
-deleteRow.setAttribute('id', 'deleteButton');
 const submitButton = document.getElementById("submit-button");
 submitButton.addEventListener('click', userData);
 
@@ -13,16 +11,34 @@ function userData(e) {
   const paid = amountPaid.value;
   const item = itemPurchased.value;
   const date = datePurchased.value;
+  const deleteRow = document.createElement('button');
+  deleteRow.setAttribute('id', 'deleteButton');
+  deleteRow.textContent = 'Delete';
   const tableData = document.getElementById("table-body");
   let newRow = `
     <tr>
       <td>${type}</td>
-      <td>${paid}</td>
+      <td>${`$ ${paid}`}</td>
       <td>${item}</td>
       <td>${date}</td>
-      <td>${deleteRow}</td>
     </tr>
 `
-  tableData.innerHTML += newRow;
 
+  if (type !== 'Select Payment' || date !== '' || paid !== '' || item !== '') {
+    tableData.innerHTML += newRow;
+    tableData.appendChild(deleteRow);
+    submitButton.addEventListener('click', resetForm());
+  } else {
+    alert('Please enter an expense');
+  }
+}
+
+function resetForm() {
+  form = document.getElementById('form-data');
+  form.reset();
+}
+
+function deleteExpenseRow(e) {
+  let deleteData = e.target.parentElement;
+  deleteData.remove();
 }
